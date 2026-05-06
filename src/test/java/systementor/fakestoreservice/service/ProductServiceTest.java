c90d3a272001703e1d6e089b9c8645d4f1b8df61
Yahya9102 <yaya_hussein@hotmail.com>
2026-05-06 10:28:30 +0200

    add test for ProductService getAllProducts

---
+++ src/test/java/systementor/fakestoreservice/service/ProductServiceTest.java
@@ -0,0 +1,47 @@
+package systementor.fakestoreservice.service;
+
+import org.junit.jupiter.api.Test;
+import systementor.fakestoreservice.model.Product;
+import systementor.fakestoreservice.repository.ProductRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getAllProducts() {
+
+        // ARRANGE
+        ProductRepository repository = mock(ProductRepository.class);
+
+        Product product1 = new Product();
+        product1.setTitle("test product 1");
+
+        Product product2 = new Product();
+        product2.setTitle("test product 2");
+
+        List<Product> fakeProducts = List.of(product1, product2);
+
+        when(repository.findAll()).thenReturn(fakeProducts);
+
+        ProductService productService = new ProductService(repository);
+
+
+        // ACT
+        List<Product> result = productService.getAllProducts();
+
+        // ASSERT
+
+        assertEquals(2, result.size());
+        assertEquals("test product 1", result.get(0).getTitle());
+        assertEquals("test product 2", result.get(1).getTitle());
+
+
+        verify(repository).findAll();
+
+
+    }
+}
