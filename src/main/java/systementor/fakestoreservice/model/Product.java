d1c2aecaefa1499e991ea9ce6061aba69b128eca
Yahya9102 <yaya_hussein@hotmail.com>
2026-05-10 18:02:07 +0200

    fix products so it returns an identical to fakestore

--- src/main/java/systementor/fakestoreservice/model/Product.java
+++ src/main/java/systementor/fakestoreservice/model/Product.java
@@ -1,22 +1,46 @@
 package systementor.fakestoreservice.model;
 
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 @Entity
 public class Product {
 
     @Id
     private long id;
+
     private String title;
+
     private Double price;
+
+    @Lob
+    @Column(length = 5000)
     private String description;
+
     private String category;
+
+    @Column(length = 1000)
     private String image;
 
+    @Embedded
+    private Rating rating;
 
-    public Product() {}
+    public Product() {
+    }
+
+    public Product(long id, String title, Double price, String description,
+                   String category, String image, Rating rating) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.image = image;
+        this.rating = rating;
+    }
 
     public long getId() {
         return id;
@@ -64,5 +88,13 @@ public class Product {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
     }
 }
---
+++ src/main/java/systementor/fakestoreservice/model/Rating.java
@@ -0,0 +1,35 @@
+package systementor.fakestoreservice.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Rating {
+
+    private Double rate;
+
+    private Integer count;
+
+    public Rating() {
+    }
+
+    public Rating(Double rate, Integer count) {
+        this.rate = rate;
+        this.count = count;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
